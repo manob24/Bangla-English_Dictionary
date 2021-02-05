e77cd1d9d831259800090b5ad21aab860b7dc98d
wakil <wakilurislam@gmail.com>
2021-02-05 21:30:54 +0600

    copied from previous project

---
+++ lib/pages/home.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  bool isSearching = false;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: !isSearching? Text("Bangla-English Dictionary"):
+        TextField(
+          decoration: InputDecoration(
+            icon: Icon(Icons.search),
+            hintText: "Enter word",
+          ),
+        ),
+        actions: [
+          !isSearching?
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: (){
+              setState(() {
+                isSearching = true;
+              });
+            },
+          ):
+          IconButton(
+            icon: Icon(Icons.cancel_rounded),
+            onPressed: (){
+              setState(() {
+                isSearching = false;
+              });
+            },
+          )
+        ],
+
+      ),
+      body: SafeArea(child: Text("home screen")),
+    );
+  }
+}
