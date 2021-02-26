c03bd69d56821063656240cc59d889427159d93e
wakil <wakilurislam@gmail.com>
2021-02-26 22:17:27 +0600

    bug fixed in hashing

--- lib/fetchData.dart
+++ lib/fetchData.dart
@@ -10,9 +10,9 @@ class FetchData{
   List data;    //parsed json of words
   List<Word> words;   //decoded from json(data)
   final random = new Random();  //to get random numbers for a,b
-  int m = 17000;    //number of slots
+  final int m = 17000;    //number of slots
   int p = 2038074743; // a prime number which will be used to mod in universal hash
-  int bigPrime = 2760727302517;
+  int bigPrime = 29996224275833;
   int a, b;  // random numbers which used in primary universal hashing,
   List<List<int>> secondaryHashData = new List.generate(17000, (index) => []); //m,a,b for secondary hash
 
@@ -38,14 +38,14 @@ class FetchData{
     }
     return result;
   }
-
   //finding key of a string
   int getKey(String name){
     name = formatWord(name);
     int key = 1;
+    int k = 0;
     int len = name.length;
     for(int i = len-1; i>=0; --i){
-      key = (key + ((name.codeUnitAt(i)-97)*pow(26, i))%p)%p;
+      key = (key + ((name.codeUnitAt(i)-96)*pow(27, k++))%p)%p;
     }
     return key;
   }
@@ -70,25 +70,41 @@ class FetchData{
       int bs = random.nextInt(p);
 
       List<Word> copied = List.from(hashedList[i]);
+//      print("$hashedList[i].length");
       hashedList[i].clear();
       int n = copied.length;
+//      print("$n");
       int mj = n*n;
-      hashedList[i].length = mj;
+      while(mj>0) {
+        hashedList[i].clear();
+        hashedList[i].length = mj;
 
+        as = 1+random.nextInt(p-1);
+        bs = random.nextInt(p);
+//        print("as $as bs $bs mj $mj");
+        bool done = true;
+        for (Word word in copied) {
+          int k = getKey(word.en);
+          int key = ((as * k + bs) % p) % mj;
+          if (hashedList[i][key] != null) { //checking collision
+            if(formatWord(word.en) == formatWord(hashedList[i][key].en)){
+              done = true;
+              break;
+            }
+            done = false;
+            break;
+          }
+          hashedList[i][key] = word;
+        }
+        if(done)
+          break;
+      }
       secondaryHashData[i].length = 3;
       secondaryHashData[i][0] = mj;
       secondaryHashData[i][1] = as;
       secondaryHashData[i][2] = bs;
-
-      for(Word word in copied){
-        int k = getKey(word.en);
-        int key = ((as*k+bs)%p)%mj;
-        while(hashedList[i][key]!=null){  //linear probing
-          key = (key+1)%mj;
-        }
-        hashedList[i][key] = word;
-      }
     }
+    print("done");
   }
   String formatWord(String word){
     word = word.replaceAll(new RegExp(r'[^\w\\s]+'),'');
@@ -106,23 +122,12 @@ class FetchData{
     int skey;
     String foundWord;
     Word found;
-    if(mj == 1){
-      found = hashedList[pkey][0];
+    if(mj!=0) {
+      skey = ((as * k + bs) % p) % mj;
+      found = hashedList[pkey][skey];
+    }
+    if(found!=null)
       foundWord = formatWord(found.en);
-    }
-    else if(mj > 1){
-      skey = ((as*k+bs)%p)%mj;
-      while(hashedList[pkey][skey]!=null){
-        found = hashedList[pkey][skey];
-        foundWord = formatWord(found.en);
-        if( foundWord!= word){
-          skey = (skey+1)%mj;
-        }
-        else{
-          break;
-        }
-      }
-    }
     if(mj == 0 || found == null || foundWord != word){
       return null;
     }
